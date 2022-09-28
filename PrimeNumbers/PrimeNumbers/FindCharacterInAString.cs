978a748797083c1e3cbb0d4e2d995945369af960
vineethrk <vineethrk@moback.com>
2022-09-28 09:10:19 +0530

    Task

---
+++ PrimeNumbers/PrimeNumbers/FindCharacterInAString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_task
+{
+    internal class FindCharacterInAString
+    {
+        public void FindString()
+        {
+
+            Console.WriteLine("Enter the string or word : \n");
+            string mainString = Console.ReadLine();
+            Console.WriteLine("Enter Char to find: \n");
+            //converting string to char
+            char find = Convert.ToChar(Console.ReadLine());
+            //contains is a predefined string function
+            if (mainString.Contains(find))
+            {
+                Console.WriteLine("{0} is there in given {1}", find, mainString);
+            }
+            else
+                Console.WriteLine("{0} is not there in given {1}", find, mainString);
+        }
+    }
+}
+
+    
+
--- PrimeNumbers/PrimeNumbers/Program.cs
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -1,14 +1,17 @@
-﻿using PrimeNumbers;
+﻿using Assignment_task;
+using MasteringCSharp.Assignment;
 
-namespace PrimeNumbers
+namespace MasteringCSharp
 {
     public class Program
     {
 
         public static void Main(string[] args)
         {
-            var obj = new FindPrimeNumber();
-            obj.IsPrimeNumber();
+            //var obj = new FindPrimeNumber();
+            //obj.IsPrimeNumber();
+            var obj = new FindCharacterInAString();
+            obj.FindString();
         }
     }
 }
