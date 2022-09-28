2a63ab60e8d8f041e2347db1300d465af7614e75
vineethrk <vineethrk@moback.com>
2022-09-28 08:36:07 +0530

    Assignment task

---
+++ MasteringCSharp/MasteringCSharp/Assignment/FindCharacterInAString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasteringCSharp.Assignment
+{
+    internal class FindCharacterInAString
+    {
+        public void FindString()
+        {
+
+            Console.WriteLine("Enter the string or word : \n");
+            string mainString = Console.ReadLine();
+            Console.WriteLine("Enter Char to find: \n");
+            //converting string to char
+            char find =  Convert.ToChar( Console.ReadLine());
+            //contains is a predefined string function
+            if (mainString.Contains(find))
+            {
+                Console.WriteLine("{0} is there in given {1}", find, mainString);
+            }
+            else
+                Console.WriteLine("{0} is not there in given {1}", find, mainString);
+        }
+    }
+}
