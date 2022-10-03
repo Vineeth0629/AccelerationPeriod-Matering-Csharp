2ac585f76c3c7c43d7c352475f85a8075e41dcaf
vineethrk <vineethrk@moback.com>
2022-10-03 23:45:02 +0530

    Adding interface for Bank

---
+++ Bankdetails/Bankdetails/BankDetails.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Bankdetails
+{
+
+    internal class Bank : IBankDetails
+    {
+        //variable dec --private
+        string bankName;
+        string branchName;
+        //property dec
+
+        public string BankName
+        {
+            get { return bankName; }
+            set { bankName = value; }
+        }
+        public string BranchName
+        {
+            get { return branchName; }
+            set { branchName = value; }
+        }
+        public void getBankDetails(int id)
+        {
+            //set the value to pro
+
+            Console.WriteLine("bank Details");
+            Console.WriteLine("Bank name: {0}", bankName);
+            Console.WriteLine("Branch name: {0}", branchName);
+
+        }
+
+      
+      
+
+        public int updateBankDetails()
+        {
+            return 0;
+        }
+    }
+
+}
+
---
+++ Bankdetails/Bankdetails/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Bankdetails
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            var b= new Bank();
+            //b.getBankDetails();
+            b.BankName = Console.ReadLine();
+            b.BranchName = Console.ReadLine();
+            Console.WriteLine(b.BankName);
+            b.getBankDetails();
+
+        }
+    }
+}
