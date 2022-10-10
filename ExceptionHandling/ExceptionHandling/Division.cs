7a9e2c4d71a138b02c2e4d9d109cd0e45cbf7b03
vineethrk <vineethrk@moback.com>
2022-10-10 23:46:03 +0530

    Implementation of ExceptionHandling & Indexer

---
+++ ExceptionHandling/ExceptionHandling/Division.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Division
+    {
+        private string[] val = new string[3];
+        // this - is the keyword having a parameters list
+        public string this[int index]
+        {
+
+            get
+            {
+
+                return val[index];
+            }
+
+
+            set
+            {
+
+                val[index] = value;
+            }
+        }
+
+        public int devideTwoNumbers(int a, int b)
+        {
+            int c = 0;
+            try
+            {
+                 c = a / b;
+            }
+            catch(DivideByZeroException exception)
+            {
+                throw exception;
+            }
+            
+            catch(Exception ex)
+            {
+
+                return 0;
+            }
+            return c;
+        }
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Division d = new Division();
+            d[0] = "Hello";
+            d[1] = "Bangalore";
+            d[2] = "Vinny";
+            d.devideTwoNumbers(23, 0);
+        }
+    }
+}
